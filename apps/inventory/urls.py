114c68424db745a4e9c989aedb62609173692552
Ashikunnabi <ashikunnabituhin@gmail.com>
2023-09-25 20:21:19 +0600

    inventory dashboard api added

--- apps/inventory/urls.py
+++ apps/inventory/urls.py
@@ -5,5 +5,6 @@ from .views import *
 app_name = 'inventory'
 
 urlpatterns = [
+    path('api/', include('apps.inventory.dashboard_api.urls'), name='dashboard_api'),
     path('api/', include('apps.inventory.api.urls'), name='api'),
 ]
