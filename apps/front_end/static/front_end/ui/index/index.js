a5506ba2282c5d27d14b87ec5537316b06d6f5d2
Ashikunnabi <ashikunnabituhin@gmail.com>
2023-11-25 09:14:00 +0600

    table number store at localstorage

--- apps/front_end/static/front_end/ui/index/index.js
+++ apps/front_end/static/front_end/ui/index/index.js
@@ -5,3 +5,20 @@ import { menuModule } from "../components/menuModule.js";
 // categoryModule.getCategories(1);
 menuModule.getMenus();
 menuModule.getSearchItems();
+
+
+// store table number in local storage with 5 hours expiry time
+function storeTableNumber() {
+    // Get the URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Check if the 'table' parameter exists
+    if (urlParams.has('table')) {
+        // Get the value of the 'table' parameter
+        const tableValue = urlParams.get('table');
+        setLocalWithExpiry("table_uuid", tableValue, 5*60)
+    } else {
+        console.log('Table parameter not found.');
+    }
+}
+storeTableNumber()
